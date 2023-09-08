2d3a082eff16a0f20bdf7ecf3b44ef4e0c7010c2
V1tazy <132578253+V1tazy@users.noreply.github.com>
2023-09-08 12:26:08 +0900

    123

--- scripts/global.js
+++ scripts/global.js
@@ -1,6 +1,24 @@
 function global_init() {
     var cumvas = document.getElementById("main");
 
+    new KeyControls;
     cumvas.width = window.innerWidth;
     cumvas.height = window.innerHeight;
+}
+
+class KeyControls{
+    constructor( keyList = ['KeyW', 'KeyA', 'KeyD', 'KeyS']) {
+        this.keyList = keyList;
+
+        this.keys = {};
+
+        addEventListener('keydown', e => this.changeState(e));
+        addEventListener('keyup', e => this.changeState(e));
+
+    }
+    changeState(e){
+        if(!this.keyList.includes(e.code))
+        this.keys[e.code] = e.type == 'keydown' ? true : false
+        console.log(this.keys);
+    }
 }
